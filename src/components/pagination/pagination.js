479d5c9e5e1b8c72556c7f4312c4479bf577ff29
Francisco Villar Salvador <franvlive@gmail.com>
2022-02-13 19:07:07 +0100

    Diseño de la paginación

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,8 +1,16 @@
+import "./pagination.scss";
+
 const Pagination = ({ showNexts, showPrevious, offset }) => {
   return (
     <section className="pagination">
-      {offset !== 0 && <button onClick={showPrevious}>Previous</button>}
-      <button onClick={showNexts}>Next</button>
+      {offset !== 0 && (
+        <button className="pagination__button" onClick={showPrevious}>
+          Previous
+        </button>
+      )}
+      <button className="pagination__button" onClick={showNexts}>
+        Next
+      </button>
     </section>
   );
 };
